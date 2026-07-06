cb4e01ed0032b9747135fc20f397a772f1602daa
christgomis15 <christgomis15@gmail.com>
2026-07-06 09:11:19 +0000

    feat: icone app simplifiee en monogramme N dore sur fond noir (plus lisible en petit)

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -1,14 +1,9 @@
 import { ImageResponse } from 'next/og'
-import { readFileSync } from 'fs'
-import { join } from 'path'
 
 export const size = { width: 180, height: 180 }
 export const contentType = 'image/png'
 
 export default function AppleIcon() {
-  const logoData = readFileSync(join(process.cwd(), 'public/img/nostalgie-logo.png'))
-  const logoSrc = `data:image/png;base64,${logoData.toString('base64')}`
-
   return new ImageResponse(
     (
       <div
@@ -19,13 +14,19 @@ export default function AppleIcon() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          padding: '26px',
         }}
       >
-        <img
-          src={logoSrc}
-          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-        />
+        <span
+          style={{
+            fontFamily: 'Georgia, serif',
+            fontWeight: 900,
+            fontSize: 120,
+            color: '#D4A843',
+            lineHeight: 1,
+          }}
+        >
+          N
+        </span>
       </div>
     ),
     { ...size }
--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,14 +1,9 @@
 import { ImageResponse } from 'next/og'
-import { readFileSync } from 'fs'
-import { join } from 'path'
 
 export const size = { width: 512, height: 512 }
 export const contentType = 'image/png'
 
 export default function Icon() {
-  const logoData = readFileSync(join(process.cwd(), 'public/img/nostalgie-logo.png'))
-  const logoSrc = `data:image/png;base64,${logoData.toString('base64')}`
-
   return new ImageResponse(
     (
       <div
@@ -19,13 +14,19 @@ export default function Icon() {
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          padding: '72px',
         }}
       >
-        <img
-          src={logoSrc}
-          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
-        />
+        <span
+          style={{
+            fontFamily: 'Georgia, serif',
+            fontWeight: 900,
+            fontSize: 340,
+            color: '#D4A843',
+            lineHeight: 1,
+          }}
+        >
+          N
+        </span>
       </div>
     ),
     { ...size }
